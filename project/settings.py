5e65592d5e6221ca592ad6b40811b0f4cfd84897
wmsexton <wmsexton@btinternet.com>
2024-10-14 13:53:38 +0100

    Updated CORS settings

--- project/settings.py
+++ project/settings.py
@@ -59,9 +59,32 @@ MIDDLEWARE = [
 
 # CORS and CSRF settings
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://wedding-back-end-ga-32f0d464c773.herokuapp.com",
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
