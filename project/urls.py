d301c3613da8eef619cbe0d0cbdaad32c328ba0a
wmsexton <wmsexton@btinternet.com>
2024-10-14 16:09:40 +0100

    Fix enter_password view and URL configuration

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import enter_password
-from .views import set_csrf_token
-
+from .views import enter_password, set_csrf_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/enter_password/', enter_password.as_view(), name='enter_password'),
+    path('api/enter_password/', enter_password, name='enter_password'),  # Remove .as_view()
     path('api/csrf_cookie/', set_csrf_token, name='set_csrf_token'),
     path('api/guests/', include('guests.urls')),
     path('api/rsvp/', include('rsvp.urls')),
