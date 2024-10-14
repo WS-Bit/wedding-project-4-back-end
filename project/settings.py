6338b61e354da979be625647f8ac4ca46dee9646
wmsexton <wmsexton@btinternet.com>
2024-10-14 13:17:34 +0100

    added functionality to the front end

--- project/settings.py
+++ project/settings.py
@@ -61,12 +61,12 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "https://wedding-back-end-ga-32f0d464c773.herokuapp.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "https://wedding-back-end-ga-32f0d464c773.herokuapp.com",
 ]
 
 CSRF_COOKIE_SAMESITE = 'Lax'
